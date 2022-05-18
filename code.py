c606067fb7e587c5f28edc42a78750c96aadcf5c
Milena Sviatek <81256013+milasvi@users.noreply.github.com>
2022-05-18 11:04:20 -0300

    Update code.py
    Problemas resolvidos

--- code.py
+++ code.py
@@ -7,13 +7,14 @@ while True:
     LadoB = int(input("Informe o valor do segundo lado:"))
     LadoC = int(input("Informe o valor do terceiro lado:"))
 
-    if LadoA < LadoB + LadoC:
+    if LadoA < LadoB + LadoC and LadoC < LadoA + LadoB and LadoB < LadoC + LadoA:
         print(f'Os lados {LadoA},{LadoB},{LadoC} formam um triângulo!')
     else:
-        print('Esses lados não formam um triângulo, pois LadoA > LadoB + LadoC!\nQue pena!')
+        print('Esses lados não formam um triângulo, pois um lado não é menor que a soma dos outros dois.')
     repeticao = input('Deseja informar novos valores? (Digite sim ou nao)')
+    while repeticao != 'sim' and repeticao != 'nao':
+        repeticao = input('Não entendi, digite apenas sim ou nao:')
     if repeticao == 'nao':
         print('Obrigade por usar o Triangulo Descobrinator! \nAté mais!')
         break
-    while repeticao != 'sim':
-        repeticao = input('Não entendi, digite apenas sim ou nao:')
+    
